038a36826604e49e3b8a9d7efbf96cb17a20e4dd
Austin Thaldorf-Huelsbeck <75189212+austinthaldorfhuelsbeck@users.noreply.github.com>
2024-04-14 10:47:55 -0700

    with hero text

---
+++ src/app/_components/hero-slider.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { heroImages } from "@/lib/constants";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+export function HeroSlider() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  // Rotate through slides
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentSlide((prev) => (prev + 1) % heroImages.length);
+    }, 4000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <header>
+      {heroImages.map((_, idx) => (
+        <Image
+          key={idx}
+          src={heroImages[idx]}
+          width={1600}
+          height={900}
+          alt="Austin + Emily Creative"
+          className={cn(
+            "object-cover w-full h-full absolute transition-all duration-2000 ease-in-out ",
+            idx === currentSlide ? "opacity-100" : "opacity-0"
+          )}
+          style={{ height: "calc(100vh - 48px)", filter: "brightness(70%)" }}
+        />
+      ))}
+      <aside className="absolute bottom-0 text-primary mx-10 my-14 italic flex flex-col gap-2">
+        <h1 className="text-6xl md:text-8xl">LOVE Stories</h1>
+        <div className="flex gap-2 items-center w-full">
+          <Image src="/assets/one-star.png" width={25} height={25} alt="Star" />
+          <div className="h-1 flex-1 border-t border-primary" />
+          <p className="font-bold tracking-wider flex-1">
+            crafted by you, captured by us
+          </p>
+        </div>
+      </aside>
+    </header>
+  );
+}
---
+++ src/lib/constants.ts
@@ -0,0 +1,13 @@
+import heroSlider1 from "../../public/images/hero-slider-1.jpg";
+import heroSlider2 from "../../public/images/hero-slider-2.jpg";
+import heroSlider3 from "../../public/images/hero-slider-3.jpg";
+import heroSlider4 from "../../public/images/hero-slider-4.jpg";
+import heroSlider5 from "../../public/images/hero-slider-5.jpg";
+
+export const heroImages = [
+  heroSlider1,
+  heroSlider2,
+  heroSlider3,
+  heroSlider4,
+  heroSlider5,
+];
